8957f17ee5294ccc67850f1dc0cbc1aec65ba49a
ping <yangpingca@hotmail.com>
2019-01-18 00:38:44 -0500

    first version of new math.html

--- js/math.js
+++ js/math.js
@@ -1,71 +1,168 @@
 class CalcItem {
-    constructor(name, operator, initNumber, isRight) {
+    constructor(name, operator, generateQuestion) {
         this.name = name;
         this.operator = operator;
         this.level = -1;
 
-        this.initNumber = initNumber; // function
-        this.isRight = isRight; // function
+        this.generateQuestion = generateQuestion; // function
     }
 }
 
 class QuestionItem {
-    constructor(num, item) {
-        this.num = num;
-        this.item = item;
+    constructor(calcItem, n1, n2, r) {
+        this.calcItem = calcItem;
+        this.num1 = n1;
+        this.num2 = n2;
+        this.answer = r;
     }
 }
 
 var availCalcItems = [
     new CalcItem("addition", '+',
         function () {
-            var m = 10;
-            if (this.level >= 2)
-                m = 1000;
-            else if (this.level >= 1)
-                m = 100;
-            else
-                m = 10;
+            var n1, n2;
 
-            var n1 = Math.floor((Math.random() * m) + 1);
-            var n2 = Math.floor((Math.random() * m) + 1);
-            return [n1, n2];
-        },
-        function (n1, n2, r) {
-            return (n1 + n2) == r;
+            if (this.level <= 0) {
+                n1 = Math.floor((Math.random() * 9) + 1);
+                n2 = Math.floor((Math.random() * 9) + 1);
+            } else if (this.level <= 1) {
+                n1 = Math.floor((Math.random() * 50) + 1);
+                n2 = Math.floor((Math.random() * 50) + 1);
+                if (n1 > n2) {
+                    n1 = n1 - n2;
+                } else {
+                    n2 = n2 - n1;
+                }
+            } else if (this.level <= 2) {
+                n1 = Math.floor((Math.random() * 99) + 1);
+                n2 = Math.floor((Math.random() * 99) + 1);
+                if (n1 > n2) {
+                    n1 = n1 - n2;
+                } else {
+                    n2 = n2 - n1;
+                }
+            } else if (this.level <= 3) {
+                n1 = Math.floor((Math.random() * 99) + 1);
+                n2 = Math.floor((Math.random() * 99) + 1);
+            } else if (this.level <= 4) {
+                n1 = Math.floor((Math.random() * 500) + 1);
+                n2 = Math.floor((Math.random() * 500) + 1);
+                if (n1 > n2) {
+                    n1 = n1 - n2;
+                } else {
+                    n2 = n2 - n1;
+                }
+            } else {
+                n1 = Math.floor((Math.random() * 999) + 1);
+                n2 = Math.floor((Math.random() * 999) + 1);
+            }
+
+            return new QuestionItem(this, n1, n2, n1 + n2);
         }
     ),
     new CalcItem("subtraction", '-',
         function () {
+            var n1, n2;
 
-            var n1 = Math.floor((Math.random() * 9) + 1);
-            var n2 = Math.floor((Math.random() * 9) + 1);
-            return [n1, n2];
-        },
-        function (n1, n2, r) {
-            return (n1 - n2) == r;
+            if (this.level <= 0) {
+                n1 = Math.floor((Math.random() * 9) + 1);
+                n2 = Math.floor((Math.random() * 9) + 1);
+            } else if (this.level <= 1) {
+                n1 = Math.floor((Math.random() * 50) + 1);
+                n2 = Math.floor((Math.random() * 50) + 1);
+            } else if (this.level <= 2) {
+                n1 = Math.floor((Math.random() * 99) + 1);
+                n2 = Math.floor((Math.random() * 99) + 1);
+            } else if (this.level <= 3) {
+                n1 = Math.floor((Math.random() * 199) + 1);
+                n2 = Math.floor((Math.random() * 199) + 1);
+            } else if (this.level <= 4) {
+                n1 = Math.floor((Math.random() * 500) + 1);
+                n2 = Math.floor((Math.random() * 500) + 1);
+            } else {
+                n1 = Math.floor((Math.random() * 999) + 1);
+                n2 = Math.floor((Math.random() * 999) + 1);
+            }
+
+            if (n1 < n2) {
+                var t = n1; n1 = n2; n2 = t;
+            }
+
+            return new QuestionItem(this, n1, n2, n1 - n2);
         }
     ),
     new CalcItem("multiplication", '*',
         function () {
-            var num1, num2;
-            num1 = Math.floor((Math.random() * 9) + 1);
-            num2 = Math.floor((Math.random() * 9) + 1);
-            return [num1, num2];
-        },
-        function (n1, n2, r) {
-            return (n1 * n2) == r;
+            var n1, n2;
+
+            if (this.level <= 0) {
+                n1 = Math.floor((Math.random() * 9) + 1);
+                n2 = Math.floor((Math.random() * 9) + 1);
+            } else if (this.level <= 1) {
+                n1 = Math.floor((Math.random() * 49) + 1);
+                n2 = Math.floor((Math.random() * 9) + 1);
+            } else if (this.level <= 2) {
+                n1 = Math.floor((Math.random() * 99) + 1);
+                n2 = Math.floor((Math.random() * 99) + 1);
+            } else if (this.level <= 3) {
+                n1 = Math.floor((Math.random() * 99) + 1);
+                n2 = Math.floor((Math.random() * 99) + 1);
+            } else if (this.level <= 4) {
+                n1 = Math.floor((Math.random() * 500) + 1);
+                n2 = Math.floor((Math.random() * 500) + 1);
+                if (n1 > n2) {
+                    n1 = n1 - n2;
+                } else {
+                    n2 = n2 - n1;
+                }
+            } else {
+                n1 = Math.floor((Math.random() * 999) + 1);
+                n2 = Math.floor((Math.random() * 999) + 1);
+            }
+
+            return new QuestionItem(this, n1, n2, n1 * n2);
         }
     ),
     new CalcItem("division", '/',
         function () {
-            var num1, num2;
-            num1 = Math.floor((Math.random() * 9) + 1);
-            num2 = Math.floor((Math.random() * 9) + 1);
-            return [num1, num2];
-        },
-        function (n1, n2, r) {
-            return (n1 / n2) == r;
+            var n1, n2;
+
+            if (this.level <= 0) {
+                n1 = Math.floor((Math.random() * 9) + 1);
+                n2 = Math.floor((Math.random() * 9) + 1);
+            } else if (this.level <= 1) {
+                n1 = Math.floor((Math.random() * 50) + 1);
+                n2 = Math.floor((Math.random() * 50) + 1);
+                if (n1 > n2) {
+                    n1 = n1 - n2;
+                } else {
+                    n2 = n2 - n1;
+                }
+            } else if (this.level <= 2) {
+                n1 = Math.floor((Math.random() * 99) + 1);
+                n2 = Math.floor((Math.random() * 99) + 1);
+                if (n1 > n2) {
+                    n1 = n1 - n2;
+                } else {
+                    n2 = n2 - n1;
+                }
+            } else if (this.level <= 3) {
+                n1 = Math.floor((Math.random() * 99) + 1);
+                n2 = Math.floor((Math.random() * 99) + 1);
+            } else if (this.level <= 4) {
+                n1 = Math.floor((Math.random() * 500) + 1);
+                n2 = Math.floor((Math.random() * 500) + 1);
+                if (n1 > n2) {
+                    n1 = n1 - n2;
+                } else {
+                    n2 = n2 - n1;
+                }
+            } else {
+                n1 = Math.floor((Math.random() * 999) + 1);
+                n2 = Math.floor((Math.random() * 999) + 1);
+            }
+
+            return new QuestionItem(this, n1, n2, n1 / n2);
         }
     )
 ];
