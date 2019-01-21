3b87fd39263aa8a7768260a93e989f11b3528e13
ping <yangpingca@hotmail.com>
2019-01-20 20:48:13 -0500

    update math.js

--- js/math.js
+++ js/math.js
@@ -103,7 +103,7 @@ var availCalcItems = [
                 n2 = Math.floor((Math.random() * 9) + 1);
             } else if (this.level <= 3) {
                 n1 = Math.floor((Math.random() * 99) + 1);
-                n2 = Math.floor((Math.random() * 99) + 1);
+                n2 = Math.floor((Math.random() * 9) + 1);
             } else if (this.level <= 4) {
                 n1 = Math.floor((Math.random() * 99) + 1);
                 n2 = Math.floor((Math.random() * 99) + 1);
@@ -123,36 +123,32 @@ var availCalcItems = [
             var n1, n2;
 
             if (this.level <= 1) {
+                n1 = Math.floor((Math.random() * 5) + 1);
+                n2 = Math.floor((Math.random() * 5) + 1);
+            } else if (this.level <= 2) {
+                n1 = Math.floor((Math.random() * 9) + 1);
+                n2 = Math.floor((Math.random() * 5) + 1);
+            } else if (this.level <= 3) {
                 n1 = Math.floor((Math.random() * 9) + 1);
                 n2 = Math.floor((Math.random() * 9) + 1);
-            } else if (this.level <= 2) {
-                n1 = Math.floor((Math.random() * 50) + 1);
-                n2 = Math.floor((Math.random() * 50) + 1);
-                if (n1 > n2) {
-                    n1 = n1 - n2;
-                } else {
-                    n2 = n2 - n1;
-                }
-            } else if (this.level <= 3) {
-                n1 = Math.floor((Math.random() * 99) + 1);
-                n2 = Math.floor((Math.random() * 99) + 1);
-                if (n1 > n2) {
-                    n1 = n1 - n2;
-                } else {
-                    n2 = n2 - n1;
-                }
             } else if (this.level <= 4) {
+                n1 = Math.floor((Math.random() * 9) + 1);
+                n2 = Math.floor((Math.random() * 49) + 1);
+            } else if (this.level <= 5) {
+                n1 = Math.floor((Math.random() * 9) + 1);
+                n2 = Math.floor((Math.random() * 99) + 1);
+            } else if (this.level <= 6) {
+                n1 = Math.floor((Math.random() * 49) + 1);
+                n2 = Math.floor((Math.random() * 99) + 1);
+            } else if (this.level <= 7) {
                 n1 = Math.floor((Math.random() * 99) + 1);
                 n2 = Math.floor((Math.random() * 99) + 1);
-            } else if (this.level <= 5) {
-                n1 = Math.floor((Math.random() * 500) + 1);
-                n2 = Math.floor((Math.random() * 500) + 1);
             } else {
                 n1 = Math.floor((Math.random() * 999) + 1);
                 n2 = Math.floor((Math.random() * 999) + 1);
             }
 
-            return new QuestionItem(this, n1, n2, n1 / n2);
+            return new QuestionItem(this, n1*n2, n1, n2);
         }
     )
 ];
